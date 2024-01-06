8d0812f581305b183d8e486b384fddff7d52d459
Roland HUON <huonroland91@gmail.com>
2024-01-06 14:24:51 +0100

    ✨ basics road creation

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 //npm init
 //npm run ...(script package.json)
 //npm install --save express
-//npm install --save-dev nodemon
+//npm install --save-dev nodemon (need to reload page not the entirety of the terminal)
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
+//road : app.methode(get, send, post, delete...)(chemin, (req or res))
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World 2!');
+});
+app.get('/api/pokemons/1', (req, res) => {
+    res.send("Hello Bulbizarre!");
 });
 
 app.listen(port, () => {
