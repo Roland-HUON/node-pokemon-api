d241577ed59035957452e7891346bc6859ab9d88
Roland HUON <huonroland91@gmail.com>
2024-01-06 15:22:10 +0100

    ✨ creation road said the numbers of pokemon in the pokedex

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World 2!');
 });
+app.get('/api/pokemons', (req,res) => {
+    res.send("Il y a " + pokemons.length + " pokémons dans le pokédex, pour le moment !");
+});
 app.get('/api/pokemons/:id', (req, res) => {
     const id = req.params.id;
     const pokemon = pokemons.find(pokemon => pokemon.id == id);
