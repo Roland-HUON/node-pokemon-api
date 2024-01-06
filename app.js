4860fe6be0a2cfcbb18b8dbc500194008792904c
Roland HUON <huonroland91@gmail.com>
2024-01-06 11:08:46 +0100

    ✨ install nodemon as devdependencies

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 //npm init
 //npm run ...(script package.json)
-console.log('Node.js start!');
+//npm install --save express
+//npm install --save-dev nodemon
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
