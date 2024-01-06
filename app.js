b5d489ba70e364ff6e2fae3261a469df81b37341
Roland HUON <huonroland91@gmail.com>
2024-01-06 14:46:23 +0100

    🐛 pokemon.id cannot match with id

--- app.js
+++ app.js
@@ -3,17 +3,20 @@
 //npm install --save express
 //npm install --save-dev nodemon (need to reload page not the entirety of the terminal)
 const express = require('express');
+let pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
 
 //road : app.methode(get, send, post, delete...)(chemin, (req or res))
+// req.params.id ou req.params.name ...
 app.get('/', (req, res) => {
     res.send('Hello World 2!');
 });
 app.get('/api/pokemons/:id', (req, res) => {
     const id = req.params.id;
-    res.send("Vous avez demandé le pokemon n°" + id);
+    const pokemon = pokemons.find(pokemon => pokemon.id == id);
+    res.send("Vous avez demandé le pokémon " + pokemon.name);
 });
 
 app.listen(port, () => {
