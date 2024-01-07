464043fc2d4d692cc5686935a586a5440e7b7ee0
Roland HUON <huonroland91@gmail.com>
2024-01-07 13:19:37 +0100

    ✨ install body parser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 //npm install --save-dev nodemon (need to reload page not the entirety of the terminal)
 //npm install morgan --save-dev
 //npm install serve-favicon --save
+//postman ou insomnia pour tester
 const express = require('express');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
