52178a1d38c7b3b3f3e17afa122b7632001d334c
Roland HUON <huonroland91@gmail.com>
2024-01-10 16:54:25 +0100

    ✨ search name by url request

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,8 +1,21 @@
-const { Pokemon } = require('../db/sequelize')
+const { Pokemon } = require('../db/sequelize');
+const { Op } = require('sequelize');
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name){ //si le paramètre name est présent dans l'url(.../api/pokemons?name=...)
+      const name = req.query.name;//extraire le paramètre name de l'url
+      return Pokemon.findAll({ 
+        where: {
+          name : { //propriété name de l'objet Pokemon
+            [Op.like]: `%${name}%` //Op.like est un opérateur de sequelize qui permet de faire une recherche partielle
+          }}})
+      .then(pokemons => {
+        const message = "Il y a " + pokemons.length + " qui correspondent au terme de votre votre recherche " + name + ".";
+        res.json({ message, data: pokemons });
+      })
+    } else {
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.';
         res.json({ message, data: pokemons });
@@ -11,5 +24,6 @@ module.exports = (app) => {
         const message = 'La liste des pokémons n\'a pas pu être récupérée. Réessayez dans quelques instants.';
         res.status(500).json({ message, data: error });
       })
+    }
   })
 }
